0c2b00cbc802254b5bb3fbf5e6ea0ee54add030f
keren melamed <kerenmelamed@campus.technion.ac.il>
2023-06-11 13:21:26 +0300

    c

--- HashTable.h
+++ HashTable.h
@@ -111,6 +111,7 @@ void HashTable<Costumer>::insert(Costumer& costumer){
     }
 
     if(tree == NULL){
+        tree = new AVLTree<Costumer>*();
         tree->setRoot(newCostumer);
         currentSize++;
     }
