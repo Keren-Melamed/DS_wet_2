034721d6d88fb86bafc39cc5a9ff74d3be48a77c
keren melamed <kerenmelamed@campus.technion.ac.il>
2023-06-11 13:27:38 +0300

    g

--- HashTable.h
+++ HashTable.h
@@ -148,7 +148,7 @@ ostream& HashTable<Costumer>::print(ostream& os)
 {
     for (int i = 0; i < size; i++)
     {
-        os << " tree number " << i;
+        os << " tree number " << i << ": ";
         if(data[i] != nullptr){
             data[i]->inOrder(os, data[i]->getRoot());
         }
