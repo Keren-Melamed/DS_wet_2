21a33f203bf90d11bd9dcf64e4169fa76f703127
keren melamed <kerenmelamed@campus.technion.ac.il>
2023-06-12 20:47:08 +0300

    u

--- UFRecords.cpp
+++ UFRecords.cpp
@@ -29,7 +29,7 @@ void UFRecords::Union(int below, int above){
     ReversedNode* B = m_parents[below];
 
     //updating the parent of above to be the parent pf below
-    A->getParent()->setParent(B->getParent());
+    A->setParent(B);
 
     //change height of above to be + height of below
     A->getValue()->UpdateHeight(B->getValue()->getHeight());
