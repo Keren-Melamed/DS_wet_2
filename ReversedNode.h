5e5767ab217ee074ee0cf1e75d95e94165ca4c3d
keren melamed <kerenmelamed@campus.technion.ac.il>
2023-06-12 20:20:22 +0300

    a

--- ReversedNode.h
+++ ReversedNode.h
@@ -3,11 +3,12 @@
 
 class ReversedNode{
     private:
-        Record* m_value;
+        Record *m_value;
         ReversedNode* m_parent;
     public:
         ReversedNode();
-        ReversedNode(Record* value){
+
+        ReversedNode(Record *value){
             m_value = value;
             m_parent = nullptr;
         }
@@ -25,6 +26,6 @@ class ReversedNode{
         }
 
         void print(std::ostream& os){
-            os << m_value->getId();
+            os << this->m_value->getId();
         }
 };
