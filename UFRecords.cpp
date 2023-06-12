f14decee41b0a6cb6df9f4a0f4f682b8802f84a0
keren melamed <kerenmelamed@campus.technion.ac.il>
2023-06-12 21:28:58 +0300

    a

--- UFRecords.cpp
+++ UFRecords.cpp
@@ -59,7 +59,7 @@ bool UFRecords::isDisjoint(int r_id1, int r_id2){
 
 void UFRecords::print(std::ostream& os){
     int i = 0;
-    while(m_parents[i] != nullptr){
+    while(m_parents[i]->getParent() != nullptr){
         os << " group number " << i << "\n";
         ReversedNode* temp = m_parents[i];
         os << " with records ";
