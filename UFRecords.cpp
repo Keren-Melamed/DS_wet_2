c09709b8786ba6ce547e8aa76e7bfa40ab31f80a
keren melamed <kerenmelamed@campus.technion.ac.il>
2023-06-17 17:50:52 +0300

    c

--- UFRecords.cpp
+++ UFRecords.cpp
@@ -98,9 +98,10 @@ void UFRecords::Union(int r_id1, int r_id2){
 
     int help = r_id1;
     while(m_parents[help] != -1){
-        m_sizes[m_parents[r_id1]] +=m_sizes[r_id2];
+        m_sizes[m_parents[help]] +=m_sizes[r_id2];
         help = m_parents[help];
     }
+
     m_sizes[r_id2] += m_sizes[r_id1];
 
     m_parents[r_id1] = r_id2;
