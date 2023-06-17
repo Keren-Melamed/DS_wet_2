e83740baa65c0712435f43653d691faaae1c2e39
keren melamed <kerenmelamed@campus.technion.ac.il>
2023-06-17 19:09:42 +0300

    c

--- UFRecords.cpp
+++ UFRecords.cpp
@@ -146,7 +146,7 @@ void UFRecords::printAllParents(std::ostream& os){
 }
 
 int UFRecords::getRecordHeight(int r_id){
-    return m_sizes[Find(r_id)];
+    return m_records[r_id].getHeight();
 }
     
 void UFRecords::deleteHelper(Record* records, int* parents, int* sizes){
